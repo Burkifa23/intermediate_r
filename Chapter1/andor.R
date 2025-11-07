c17445ba514c715949d3419afb3ea25c76b6feb4
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-07 16:34:12 +0000

    If Statement

--- Chapter1/andor.R
+++ Chapter1/andor.R
@@ -7,3 +7,30 @@ last < 5 | last > 10
 
 # Is last between 15 (exclusive) and 20 (inclusive)?
 last > 15 & last < 20
+
+# The social data (linkedin, facebook, views) has been created for you
+
+# linkedin exceeds 10 but facebook below 10
+linkedin > 10 & facebook < 10
+
+# When were one or both visited at least 12 times?
+linkedin >= 12 | facebook >= 12
+
+# When is views between 11 (exclusive) and 14 (inclusive)?
+views > 11 & views <= 14
+
+
+# Variables related to your last day of recordings
+medium <- "LinkedIn"
+num_views <- 14
+
+# Examine the if statement for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+}
+
+# Write the if statement for num_views
+if (num_views >15){
+  print("You are popular!")
+}
+
