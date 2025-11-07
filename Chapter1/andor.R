4ccb771f9567ffc9ec5caccca1d37f43a843cdcc
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-07 16:36:49 +0000

    Else if

--- Chapter1/andor.R
+++ Chapter1/andor.R
@@ -27,6 +27,10 @@ num_views <- 14
 # Examine the if statement for medium
 if (medium == "LinkedIn") {
   print("Showing LinkedIn information")
+} else if (medium == "Facebook") {
+  # Add code to print correct string when condition is TRUE
+  print("Showing Facebook information")
+
 } else{
   print("Unknown medium")
 }
@@ -34,6 +38,10 @@ if (medium == "LinkedIn") {
 # Write the if statement for num_views
 if (num_views >15){
   print("You are popular!")
+} else if (num_views <= 15 & num_views > 10) {
+  # Add code to print correct string when condition is TRUE
+  print("Your number of views is average")
+
 } else {
   print("Try to be more visible!")
 }
