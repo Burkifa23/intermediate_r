65ffcf2c65d5067cdbd160be156b3652785e38e5
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2025-11-08 21:02:07 +0000

    Refine pow_two()
    Enhanced pow_two() function to include optional printing of results.

--- Chapter3/myFunction.R
+++ Chapter3/myFunction.R
@@ -1,12 +1,12 @@
-# Create a function pow_two()
-pow_two <- function(x) {
-    x*x
+# Finish the pow_two() function
+pow_two <- function(x, print_info=TRUE) {
+  y <- x ^ 2
+  if(print_info){
+    print(paste(x, "to the power two equals", y))
+  }
+  return(y)
 }
-
-
-# Use the function
-pow_two(12)
-
+pow_two(2)
 
 # Create a function sum_abs()
 sum_abs <- function(x,y) {
